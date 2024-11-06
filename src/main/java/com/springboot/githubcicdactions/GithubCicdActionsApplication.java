ab9e38917a7410527377ed92671fd9725fc2ff92
Govind Mp <65946002+Govind-MP@users.noreply.github.com>
2024-11-06 22:50:28 +0530

    edited code

--- src/main/java/com/springboot/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/springboot/githubcicdactions/GithubCicdActionsApplication.java
@@ -2,14 +2,17 @@ package com.springboot.githubcicdactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 @RestController
 public class GithubCicdActionsApplication {
 
     @GetMapping("/Welcome")
-    public string welcome(){
-        return "Welocme to my world!";
+    public String welcome(){
+        return "Welcome to my world!";
     }
 
     public static void main(String[] args) {
